53c729887560a44e5a0e6a6db54a85b36222a51f
Bahodur J. Nazarov <NazarovB@HUMO.local>
2023-06-06 14:43:41 +0500

    created api func, parse api

--- cmd/activity/main.go
+++ cmd/activity/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"guthub.com/bahodurnazarov/activity/internal/api"
 	lg "guthub.com/bahodurnazarov/activity/pkg/utils"
 )
 
 func main() {
-	fmt.Println("Hello")
+	a, b, c, d, e, i, f := api.GetFromAPI()
+	fmt.Println(a, b, c, d, e, i, f)
 	lg.Server.Println("hello")
 	lg.Errl.Println("df")
 }
