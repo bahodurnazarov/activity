6558242b8d9bee9f34d4dff613b6d55257b9c28f
Bahodur J. Nazarov <NazarovB@HUMO.local>
2023-06-06 15:48:27 +0500

    created routs.go which is our main handle fail also created home file

--- cmd/activity/main.go
+++ cmd/activity/main.go
@@ -1,16 +1,7 @@
 package main
 
-import (
-	"github.com/bahodurnazarov/activity/pkg/db"
-	lg "github.com/bahodurnazarov/activity/pkg/utils"
-)
+import "github.com/bahodurnazarov/activity/internal/handler"
 
 func main() {
-	//a, b, c, d, e, i, f := api.GetFromAPI()
-	a := db.Conn()
-	_, err := a.Exec("INSERT into test VALUES ($1)", 2)
-	if err != nil {
-		lg.Errl.Fatalf("111An error occured while executing query: %v", err)
-	}
-
+	handler.Run()
 }
