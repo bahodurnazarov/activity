6be6d0fc4bf99bd67203d936115296ac77defc93
Bahodur J. Nazarov <NazarovB@HUMO.local>
2023-06-06 14:17:46 +0500

    loger working correctly

--- cmd/activity/main.go
+++ cmd/activity/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	lg "guthub.com/bahodurnazarov/activity/pkg/utils"
+)
 
 func main() {
 	fmt.Println("Hello")
+	lg.Server.Println("hello")
+	lg.Errl.Println("df")
 }
